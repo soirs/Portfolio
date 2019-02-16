82885a47c415bf3f1595443df494bc5588e020de
Frank Richard Semakula <franksemakula@gmail.com>
2019-02-16 18:44:28 +0100

    delete this file

--- static/sw.js
+++ static/sw.js
@@ -30,7 +30,7 @@
 //   return self.clients.claim();
 // });
 
-// In production, we register a service worker to serve assets from local cache.
+// In production, we register a service worker to serve assets  from local cache.
 
 // This lets the app load faster on subsequent visits in production, and gives
 // it offline capabilities. However, it also means that developers (and users)
